5b11d86ff86f612489f8f72474517edcd0b2f57e
Agyapong Derrick <dectechbusiness900@gmail.com>
2024-04-24 22:13:55 +0000

    db added

--- database/gratian_db.sql
+++ database/gratian_db.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 20, 2024 at 02:12 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 24, 2024 at 09:44 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,37 +24,179 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `price` varchar(200) NOT NULL,
+  `sub_total` decimal(10,0) NOT NULL,
+  `total` decimal(10,0) NOT NULL,
+  `user_id` int NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery_info`
+--
+
+DROP TABLE IF EXISTS `delivery_info`;
+CREATE TABLE IF NOT EXISTS `delivery_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `address1` varchar(200) NOT NULL,
+  `address2` varchar(200) NOT NULL,
+  `region` varchar(200) NOT NULL,
+  `status` enum('DELIVERED','NOT DELIVERED','','') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'NOT DELIVERED',
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `delivery_info`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_info` text,
+  `product_name` varchar(200) NOT NULL,
+  `product_quantity` varchar(200) NOT NULL,
+  `price` varchar(200) NOT NULL,
+  `product_image` varchar(200) NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `seller_id` int NOT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `transfered` tinyint(1) NOT NULL DEFAULT '0',
+  `quantity` varchar(200) DEFAULT NULL,
+  `delivery_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `price` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `quantity` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `category` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `images` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `brand` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `model` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `color` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `form_factor` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `technology` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `rating` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+CREATE TABLE IF NOT EXISTS `product_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `category_image` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`id`, `category_name`, `category_image`, `added_on`) VALUES
+(1, 'Laptops for study', 'latest-devices.jpeg', '2024-04-20 14:53:06'),
+(2, 'Computers', 'computers.jpeg', '2024-04-20 14:53:06'),
+(3, 'Gaming', 'gaming.jpg', '2024-04-20 18:45:02'),
+(4, 'Fashion', 'fashion.jpeg', '2024-04-20 18:45:02'),
+(5, 'Grooming Products\r\n', 'grooming.jpg', '2024-04-20 18:45:41'),
+(6, 'Electronics', 'electronics.jpeg', '2024-04-20 18:45:41');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `user_type` enum('SELLER','BUYER') NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_type` enum('SELLER','BUYER','ADMIN') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
